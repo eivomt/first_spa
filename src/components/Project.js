154af57aad2b725099b538502a5e1aae77b1acbc
Eivind Omtveit <e.omtveit@gmail.com>
2020-04-13 20:02:00 +0200

    changed html

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,40 +5,51 @@ import {
   mdiGithub,
   mdiPlus
 } from '@mdi/js'
+import ReactAnime from 'react-animejs'
+
 
 class Project extends Component {
+  
 
   render () {
+   
+
+    let features = Array.from(this.props.features)
+    
+    let featuresList = features.map((item) => (
+      <li>{item}</li>
+    ))
+
+    let technologies = Array.from(this.props.technologies)
+    
+    let technologiesList = technologies.map((item) => (
+      <li>{item}</li>
+    ))
+
     return (
-      <div className="project-wrapper">
-        <div className="left">
-          <div className="title">
-            <h3>{this.props.title}</h3>
-            <div className="line-wrapper">
-              <line className="title-line"></line>
-            </div>
-          </div>
-          <div className="paragraph">
-            <p>Fictotat invellicaes voloressi omnis ut oditatur, to voluptae dolor aut parum sequi cum fuga. Name la sit, commolo reperch illabori doluptas prem que velis id unt.
-            Ro occumquam quodia dolut adigenderrum quame qui blacea vererum rem endanis aspelitin excerna temque nonseque porestesed ea porro volentur solupis magnatur simus, temolorepre voluptati consequ iduciatur? Ficatat emoluptatqui unt volut ex eum fugitius quam apis et voluptatibus sequasperum sin conemporit adisquam eatiori tiasperi coreseque odis doluptatem ipsaper cipsam ut eos excepel maionet volupiti nobis quiae vel ipis eatum adissit, unt.</p>
-          </div>
-          <div className="buttons">
-            <button className="play">
-              <Icon className="project-icon" path={mdiPlay} title="play" size={1.5} />
-            </button>
-            <button className="read-more">
-            <Icon className="project-icon" path={mdiGithub} title="github" size={1.5} />
-            </button>
-            <button className="github">
-            <Icon className="project-icon" path={mdiPlus} title="readMore" size={1.5} />
-            </button>
-          </div>
+      <div className="container">
+        <div className="circle-container">
+          <div className="circle-outer"></div>
+          <div className="circle-inner"></div>
         </div>
-        <div className="line-wrapper">
-          <line className="middle-line"></line>
-        </div>
-        <div className="right">
-          <div className="image"></div>
+        <ul className="cta">
+          <button className="play">
+              <Icon className="project-icon" path={mdiPlay} title="play" size={1.5}></Icon>
+          </button>
+          <button className="github">
+              <a ></a>
+            <Icon className="project-icon" path={mdiGithub} title="github" size={1.5}></Icon>
+          </button>
+          <button className="readMore">
+              <Icon className="project-icon" path={mdiPlus} title="readMore" size={1.5}></Icon>
+          </button>
+        </ul>
+        <ul className="technologies">
+          {technologiesList}
+        </ul>
+        <div className="text">
+          <h2 className="project-title">{this.props.title}</h2>
+          <p className="summary">{this.props.summary}</p>
         </div>
       </div>
       )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
 import React, { Component, Suspense } from "react";
 import Logo from '../components/Logo.js';
 import Project from '../components/Project.js';
-import Box from '../components/Box.js';
-import Asset from '../components/Asset.js'
-import { Canvas, useFrame } from 'react-three-fiber'
 
 class Home extends Component {
   render() {
@@ -20,9 +17,8 @@ class Home extends Component {
           <circle></circle>
         </div>
         <div className="bottom-wrapper">
-          <Project title="Plus U"></Project>
-          <Project title="Forget Me Not"></Project>
-          <Project title="Sodoku"></Project>
+          <Project title="Plus U" summary="This is a web application that helps people find or create events. The goal of the project was to enable people who suffer from loneliness to easily connect with strangers and make new friends." features={["Registration", "Login", "Geocoding"]} technologies={["Ruby", "Rails", "postgreSQL", "Javascript", "HTML5", "CSS", "Sass" ]}github="https://github.com/eivomt/loner_app"></Project>
+          <Project title="Forget Me Not" summary="This is a product design project I did at KADK. The premise of the project was to choose a product to disassemble and redesign. I chose an old camera and redesigned it to be a beginners camera which would teach the user the basics of photography through experimentation" features={["Filter toggle", "Shutter speed selection", "Apperture selection", "Unique image format", "Tilt-independency"]} technologies={["Rhinoceros 5", "Grasshopper", "Adobe suite"]}></Project>
         </div>
       </div>
       
